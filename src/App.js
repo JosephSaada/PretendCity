de911b69c94a79f02e987e8da8c2347e11d0f352
Chris Tran <christran1773@gmail.com>
2023-02-10 14:45:34 -0800

    implmented carousel card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import pcCharacter from './assets/pcCharacter.png';
+import nfcPhoneIcon from './assets/nfcPhoneIcon.png';
+import mapIcon from './assets/mapIcon.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='header'>
+        <h2>Hello, Pretend City Visitor</h2>
+        <img id='pcCharacter' src={ pcCharacter }/>
+      </div>
+      <div className='carousel'>
+        <div className='card'>
+          <img className='icon' src={ nfcPhoneIcon }/>
+          <div className='cardTitle'>Guide to Tap2Play</div>
+          <div className='cardDesc'>Click to learn how to use the Tap2Play system</div>
+        </div>
+        <div className='card'>
+            <img className='icon' src={ mapIcon }/>
+          <div className='cardTitle'>Map of Museum</div>
+          <div className='cardDesc'>Click to view Tap2Play signs in the museum</div>
+        </div>
+      </div>
     </div>
   );
 }
