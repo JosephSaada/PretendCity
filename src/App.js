e9a17597f6d9b704f464d338bc40dc4bcb278c2b
Chris Tran <christran1773@gmail.com>
2023-02-10 14:58:54 -0800

    added another card on landing page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import nfcPhoneIcon from './assets/nfcPhoneIcon.png';
 import mapIcon from './assets/mapIcon.png';
 
 function App() {
+  const date = new Date();
+
   return (
     <div>
       <div className='header'>
@@ -20,6 +22,20 @@ function App() {
             <img className='icon' src={ mapIcon }/>
           <div className='cardTitle'>Map of Museum</div>
           <div className='cardDesc'>Click to view Tap2Play signs in the museum</div>
+        </div>
+      </div>
+      <h2 className='currDate'>{ date.toDateString() }</h2>
+      <div className='cardBig'>
+        <div className='cardBigTitle'>How It Works</div>
+        <div className='cardBigDesc'>
+          Tap2Play works by using NFC chips builtin
+          to devices. NFC stands for near field 
+          communication, which allows devices
+          to transfer data when they are close. 
+          Similar to tap-to-pay methods with in store
+          purchases, Tap2Play is able to reroute your
+          device to our platform. Watch the video below
+          to learn more.
         </div>
       </div>
     </div>
